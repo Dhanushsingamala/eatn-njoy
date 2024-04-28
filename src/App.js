3110984d1e1511cd8d405ae389ae033d03f855cc
Singamala Dhanush Reddy <dhanushreddy.1514@gmail.com>
2024-04-29 00:53:20 +0530

    Added Planning Comments

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+
+
+/** Planning before coding->
+ * Header
+ * -Logo
+ * -Nav Items
+ * Body
+ * -Search
+ * -RestaurentContainer
+ * Footer
+ * -CopyRight
+ * -Links
+ * -Address
+ * -Contact
+ */
+
+
+
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Header'>
+      <div className="Logo-Container">
+        <img className='logo' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqgqV9sezgYxiiPqaJ3NXXvwDbkzgXpCeBWQ&s" alt="Logo" />
+      </div>
+
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
     </div>
-  );
+  )
 }
 
-export default App;
+const AppLayout = () => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
+}
+
+export default AppLayout
